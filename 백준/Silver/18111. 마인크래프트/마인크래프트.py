7efda1f46028340c16c4f2d46209d53b191af0f5
dogdduddy <32217176+dogdduddy@users.noreply.github.com>
2022-04-01 16:17:07 +0900

    [Silver II] Title: 마인크래프트, Time: 680 ms, Memory: 127444 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -1,32 +1,29 @@
-from math import inf
 import sys
+n, m, b = map(int,sys.stdin.readline().split())
 
-N, M, B = map(int, input().split())
-ground = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+area = []
+for i in range(n):
+    area.append(list(map(int,sys.stdin.readline().split())))
 
-tall = 0
-ans = inf # 시간초과 방지를 위해 충분히 큰 수로 둠
+cnt = 1000000000000000000000000000000
+heigh = 0
 
-for i in range(257): # 땅의 높이의 최대는 256이므로 0 ~ 256까지만 탐색
+for h in range(257):
     max = 0
     min = 0
-    for j in range(N): # 가로
-        for k in range(M): # 세로
-            if ground[j][k] < i: # 블럭이 현재 높이 보다 작다면
-                min += (i - ground[j][k]) # 현재 높이가 블록 높이보다 높을 때 (min 만큼 인벤토리에서 꺼내서 채워야 함)
-
+    for i in range(n):
+        for j in range(m):
+            temp = area[i][j]
+            if temp < h:
+                min += h - temp
             else:
-                max += (ground[j][k] - i) # 블록 높이가 현재 높이보다 높을 때 (max 만큼 블록이 제거된 후 인벤토리에 들어감)
-
-    inventory = max + B # 인벤토리에 있는 총 블록수 = 현재 인벤토리에 있는 블록 + max
-
-    if inventory < min: # 전부 채울 수 없으므로 패스
+                max += temp - h
+    inv = max + b
+    if inv < min:
         continue
+    time = 2*max + min
+    if time <= cnt:
+        cnt = time
+        heigh = h
 
-    time = 2 * max + min # 블록 제거는 2초, 블록 추가는 1초
-    
-    if time <= ans: # 높이는 0 ~ 256 까지 오름차순으로 탐색하기 때문에 걸린 시간이 같아도 더 높은 높이가 출력 된다
-        ans = time
-        tall = i
-
-print(ans, tall)
+print(cnt, heigh)
